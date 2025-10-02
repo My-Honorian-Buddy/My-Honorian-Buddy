0c251d48766b913ec9171d71554a41b602b41dd7
Babolss <alaindavidsondeleon@gmail.com>
2025-10-02 18:10:47 +0800

    subject table in admin panel

---
+++ app/Filament/Resources/SubjectResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SubjectResource\Pages;
+use App\Filament\Resources\SubjectResource\RelationManagers;
+use App\Models\Subject;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Enums\ActionsPosition;
+
+class SubjectResource extends Resource
+{
+    protected static ?string $model = Subject::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-book-open';
+    protected static ?string $navigationLabel = 'Subjects';
+    protected static ?string $navigationGroup = 'Accounts';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('subj_code')->required()->label('Subject Code'),
+                Forms\Components\TextInput::make('subj_name')->required()->label('Subject Name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('subj_code')->label('Subject Code'),
+                Tables\Columns\TextColumn::make('subj_name')->label('Subject Name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                DeleteAction::make(),
+                EditAction::make(),
+            ], position: ActionsPosition::BeforeCells)
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubjects::route('/'),
+            'create' => Pages\CreateSubject::route('/create'),
+            'edit' => Pages\EditSubject::route('/{record}/edit'),
+        ];
+    }
+}
