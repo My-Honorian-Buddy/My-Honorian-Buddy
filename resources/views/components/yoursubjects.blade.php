088c593df303ff4eb765cc53f0885e3b6fd0b635
Babolss <alaindavidsondeleon@gmail.com>
2025-11-13 17:54:20 +0800

    style: change subject in your subject

--- resources/views/components/yoursubjects.blade.php
+++ resources/views/components/yoursubjects.blade.php
@@ -1,11 +1,39 @@
 @props(['pickedSubjects', 'user'])
-<div 
+<div
     class="w-full flex flex-col bg-accent rounded-md overflow-hidden border-charcoal border-2 md:max-lg:w-full sm:w-full">
-    <div class="flex bg-accent items-center w-full border-charcoal py-2">
+    <div class="flex bg-accent items-center justify-between w-full border-charcoal py-2">
         <div
-            class="font-dela flex w-full justify-start text-xl text-charcoal font-black ml-8 max-md:ml-4 max-md:text-base max-sm:text-sm">
+            class="font-dela flex w-[50%] justify-start text-xl text-charcoal font-black ml-8 max-md:ml-4 max-md:text-base max-sm:text-sm">
             Your Subjects
         </div>
+
+
+        <p onclick="showModal('changeSubjectModal')" class="w-[50%] text-right mr-8 cursor-pointer hover:underline hover:text-primary">
+            Change Subjects
+        </p>
+
+        <x-bladewind.modal-explore name="changeSubjectModal" :show_footer="false" :close_on_outside_click="true" :close_on_escape="true" size="large"
+            show_action_buttons="false">
+            <form action="{{ route('profile.change-subjects') }}" method="post">
+                @csrf
+                <div class="flex flex-col mb-2 text-left h-auto">
+                    <span class="font-bold text-2xl text-black leading-relaxed">
+                        Subject Change Confirmation
+                    </span>
+                    <span class="font-semibold text-primary text-1xl mb-8 ml-0">
+                        Changing your subjects will remove your current selections,
+                        and you will need to choose them again.
+                    </span>
+                    <button type="submit"
+                        class="p-2 border-2 border-charcoal rounded-sm text-lg
+                                    transition-all duration-600 ease-in-out hover:bg-primary hover:text-accent
+                                     bg-accent text-charcoal uppercase tracking-widest ">
+                        Change
+                    </button>
+                </div>
+            </form>
+        </x-bladewind.modal-explore>
+
     </div>
     <span class="flex mx-4 items-center">
         <span class="h-px flex-1 bg-charcoal"></span>
@@ -15,12 +43,13 @@
         @foreach ($pickedSubjects as $subject)
             <div
                 class="bg-accent flex items-center w-full py-2 px-8 max-md:flex-col max-md:items-start max-md:px-4 max-sm:px-2">
-                
+
                 <div class="grid grid-rows-1 my-7 ml-3 max-md:ml-4 max-md:my-4 max-md:w-full max-sm:ml-2 max-sm:my-3">
                     <div>
                         <p class="font-poppins font-bold text-2xl break-words max-md:text-lg max-sm:text-base">
                             {{ $subject->subj_code }} -
-                            <span class="font-semibold text-xl text-primary italic max-md:text-base max-sm:text-sm">{{ $subject->subj_name }}</span>
+                            <span
+                                class="font-semibold text-xl text-primary italic max-md:text-base max-sm:text-sm">{{ $subject->subj_name }}</span>
                         </p>
                     </div>
                 </div>
