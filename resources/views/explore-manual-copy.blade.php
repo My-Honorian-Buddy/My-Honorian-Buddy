bf192182cc4aaa132ace30940ffefb15c39f7ffe
Seshiruu <trinidadcecil22@gmail.com>
2025-11-16 02:32:49 +0800

    update: explore manual

--- resources/views/explore-manual-copy.blade.php
+++ resources/views/explore-manual-copy.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="user-id" content="{{ Auth::id() }}">
+    
     <title>My Honorian Buddy</title>
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -14,7 +15,7 @@
     <link rel="icon" href="{{ asset('/images/favicon.svg') }}" type="image/x-icon">
 
     <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
-    <link rel="stylesheet" href="burger.css">
+    <link rel="stylesheet" href="{{ asset('css/chatify/burger.css') }}">
     <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
